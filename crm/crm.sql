8249549211cfcc50eedbce06a7579c0163c1e239
Braylon Gurnell <braylon.gurnell@kellogg.northwestern.edu>
2022-04-14 02:33:00 +0000

    In-Class work

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,39 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+
+
 -- CREATE TABLES
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT
+  company TEXT
+  company_id INTEGER
+);
+
+CREATE TABLE activites (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  calls INTEGER,
+  emails INTEGER,
+  date TEXT,
+  time TEXT
+);
+
+CREATE TABLE companies (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT
+industry TEXT
+);
+
+ CREATE TABLE industries (
+     id INTEGER PRIMARY KEY AUTOINCREMENT
+     industry TEXT
+     company_id INTEGER
+ );
